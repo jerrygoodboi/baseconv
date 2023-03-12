1e53d347a65c3bdbf9070835ab62cca312716f01
Jerry Ron Sunny <rabbitron295@gmail.com>
2023-03-12 23:56:56 +0530

    new things

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
-#define BASE 2
-int main(){
-	int num,bin=0;
-scanf("%d",&num);
+int main(int argc,char **argv){
+	int num,bin=0,BASE;
+	switch(*argv[1]){
+		case 'b': 
+			BASE=2;
+			break;
+		case 'o':
+			BASE=8;
+			break;
+		default:
+			printf("That's why u r a failure\n");
+	
+	}
+num=atoi(argv[2]);
+printf("%d\n",num);
 int i=0;
 while(1){
 bin=bin*10+(num%BASE);
